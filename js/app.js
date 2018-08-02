afbb6a1037f444c70b4dd10a7ab786b882d33f32
Juliana Augusta <juli_aug@hotmail.com>
2018-08-02 15:03:52 -0300

    Mapa com pins na tela

--- js/app.js
+++ js/app.js
@@ -1,36 +1,24 @@
-var map, infoWindow;
-     function initMap() {
-       map = new google.maps.Map(document.getElementById('map'), {
-         center: {lat: -34.397, lng: 150.644},
-         zoom: 6
-       });
-       infoWindow = new google.maps.InfoWindow;
+var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 14,
+      center: new google.maps.LatLng(-23.5576413, -46.6644888),
+      mapTypeId: 'roadmap'
+    });
 
-       // Try HTML5 geolocation.
-       if (navigator.geolocation) {
-         navigator.geolocation.getCurrentPosition(function(position) {
-           var pos = {
-             lat: position.coords.latitude,
-             lng: position.coords.longitude
-           };
 
-           infoWindow.setPosition(pos);
-           infoWindow.setContent('Location found.');
-           infoWindow.open(map);
-           map.setCenter(pos);
-         }, function() {
-           handleLocationError(true, infoWindow, map.getCenter());
-         });
-       } else {
-         // Browser doesn't support Geolocation
-         handleLocationError(false, infoWindow, map.getCenter());
-       }
-     }
+  var features = restaurantes.map(function(r){
+    return {
+      position: new google.maps.LatLng(r.latitude, r.longitude),
+        type: 'info'
+      }
+    });
 
-     function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-       infoWindow.setPosition(pos);
-       infoWindow.setContent(browserHasGeolocation ?
-                             'Error: The Geolocation service failed.' :
-                             'Error: Your browser doesn\'t support geolocation.');
-       infoWindow.open(map);
-     }
+        // Create markers.
+        features.forEach(function(feature) {
+          var marker = new google.maps.Marker({
+            position: feature.position,
+            map: map
+          });
+        });
+      }
