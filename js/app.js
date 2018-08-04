37c2b29893a8ca7952a11457ef49b3ce02deec1b
Juliana Augusta <juli_aug@hotmail.com>
2018-08-04 17:30:31 -0300

    Mudanças de estilo e no arquivo data. Problemas com api do google

--- js/app.js
+++ js/app.js
@@ -1,38 +1,46 @@
+$(document).ready(function() {
+  $('.box-2').delay('2000').fadeIn('slow')
+});
+
+$(document).ready(function() {
+  $(".form-control").focusout(function(){
+       var busca = $(this).val();
+       restaurantes.map(function(r){
+         if (busca === r.type){
+           $('.container-search').append(`<img src = "${r.image}">`)
+           console.log(r);
+         }
+       })
+   });
+});
+
 $(document).ready(function(){
   $.each(restaurantes, function (index, restaurante){
     $("<img>").attr("src", restaurante.image).appendTo(".img-area");
   });
 });
 
-// $(document).ready(function() {
+// var map;
+//   function initMap() {
+//     map = new google.maps.Map(document.getElementById('map'), {
+//       zoom: 14,
+//       center: new google.maps.LatLng(-23.5576413, -46.6644888),
+//       mapTypeId: 'roadmap'
+//     });
 //
-//   $.each(restaurantes, function (index, restaurante) {
-//     $("<img>").attr("src", restaurante.image).appendTo(".img-restaurant");
-// })
-// });
-
-
-var map;
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 14,
-      center: new google.maps.LatLng(-23.5576413, -46.6644888),
-      mapTypeId: 'roadmap'
-    });
-
-
-  var features = restaurantes.map(function(r){
-    return {
-      position: new google.maps.LatLng(r.latitude, r.longitude),
-        type: 'info'
-      }
-    });
-
-        // Create markers.
-  features.forEach(function(feature) {
-    var marker = new google.maps.Marker({
-      position: feature.position,
-        map: map
-      });
-    });
-  }
+//
+//     var features = restaurantes.map(function(r){
+//     return {
+//       position: new google.maps.LatLng(r.latitude, r.longitude),
+//         type: 'info'
+//       }
+//     });
+//
+//         // Create markers.
+//   features.forEach(function(feature) {
+//     var marker = new google.maps.Marker({
+//       position: feature.position,
+//         map: map
+//       });
+//     });
+//   }
